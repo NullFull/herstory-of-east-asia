ccf522ab10f0bbb4bead8580a0cc5c12feaa7e26
constmoon <jiwoo.link@gmail.com>
2020-08-15 17:00:46 +0900

    태그 가져오는 부분 추가

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -4,19 +4,34 @@ import {useLanguageContext, getI18n} from 'hooks/language'
 import kor from 'images/Flag_of_South_Korea.svg'
 import style from './EventList.module.styl'
 
+const FilterTags = list => {
+    return list.map(item => item['Tags']).join().split(',').filter((value, index, self) => (self.indexOf(value) === index) && value.length > 0)
+}
 
 const EventList = ({events}) => {
     const {code} = useLanguageContext()
 
+const EventList = ({ events }) => {
+    const { code } = useLanguageContext()
     const [tag, setTag] = React.useState(null)
     const [country, setCountry] = React.useState(null)
 
     const countries = events.map(item => item['Country/Region']).filter((value, index, self) => self.indexOf(value) === index)
+    const tags = FilterTags(events)
 
+    const handleTag = event => {
+        const targetValue = event.target.value
+        if (targetValue !== 'All Tags') {
+            setTag(targetValue)
+        } else {
+            setTag(null)
+        }
+    }
 
-    const handleChange = event => {
-        const targetValue = event.target.value;
-        if(targetValue !== 'All Country') {
+    const handleCountry = event => {
+        const targetValue = event.target.value
+        console.log(targetValue)
+        if (!targetValue !== 'All Countries') {
             setCountry(targetValue)
         } else {
             setCountry(null)
@@ -27,11 +42,14 @@ const EventList = ({events}) => {
         <div className={style.wrapper}>
             <h2 className={style.title}>Timeline & Map</h2>
             <div className={style.selector}>
-                <select>
-                    <option>All Tags</option>
+                <select onChange={handleTag}>
+                    <option value={null}>All Tags</option>
+                    {tags.map(tag => {
+                        return <option key={tag} value={tag}>{tag}</option>
+                    })}
                 </select>
-                <select onChange={handleChange}>
-                    <option>All Country</option>
+                <select onChange={handleCountry}>
+                    <option value={null}>All Countries</option>
                     {countries.map(country => {
                         return <option key={country} value={country}>{country}</option>
                     })}
